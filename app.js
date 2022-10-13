3dcfcd5b723128c5ff56af086f894dcafd54f04c
alamindjp <alaminhossaindjp@gmail.com>
2022-10-13 23:47:19 +0600

    add update, delete, search, and pagination api

--- app.js
+++ app.js
@@ -13,51 +13,74 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    next()
+}
+
 async function run() {
     try {
         await client.connect();
         const contactCallection = client.db("address_book").collection("contact");
 
-        app.get('/contact', async (req, res) => {
-            const contactAll = await contactCallection.find({}).toArray();
-            res.send(contactAll)
-        });
-        app.post('/contact', async (req, res) => {
-            const newuser = req.body;
-            const result = await contactCallection.insertOne(newuser)
-            res.send(result)
-        });
-        app.post('/contact/bulk', async (req, res) => {
-            const newusers = req.body;
-            const result = await contactCallection.insertMany(newusers)
-            res.send(result)
-        });
-        app.put('/contact/:id', async (req, res) => {
-            const { id } = req.params;
-            const updateUser=req.body;
-            const query = { _id: ObjectId(id) }
-            const options = { upsert: true };
-            const updateDoc = {
-                $set: updateUser,
-              };
-            const result = await contactCallection.updateOne(query, updateDoc, options)
-            res.send(result)
-        });
-        app.delete('/contact/:id', async (req, res) => {
-            const { id } = req.params;
-            const query = { _id: ObjectId(id) }
-            const result = await contactCallection.deleteOne(query)
-            res.send(result)
-        });
-        app.get('/contact/:id', async (req, res) => {
+        app.get('/contact/:id', verifyJWT, async (req, res) => {
             const { id } = req.params;
             const query = { _id: ObjectId(id) }
             const result = await contactCallection.findOne(query)
             res.send(result)
         });
+        app.get('/contact/search/:name', verifyJWT, async (req, res) => {
+            const name= req.params.name;
+            const result = await contactCallection.find({name: name}).toArray();
+            res.send(result)
+        });
+        app.get('/contact', verifyJWT, async (req, res) => {
+            const page = parseInt(req.query.page)
+            const limit = parseInt(req.query.limit)
+            let contacts;
+            if (limit || page) {
+                contacts = await contactCallection.find({}).skip(page * limit).limit(limit).toArray();
+            }
+            else {
+                contacts = await contactCallection.find({}).toArray();
+            }
+            res.send(contacts)
+        });
+        app.post('/contact', verifyJWT, async (req, res) => {
+            console.log('doen')
+            const newusers = req.body;
+            const result = await contactCallection.insertOne(newusers)
+            res.send(newusers)
+        });
+        app.post('/contact/bulk', verifyJWT, async (req, res) => {
+            const newusers = req.body;
+            const result = await contactCallection.insertMany(newusers)
+            res.send(result)
+        });
+        app.put('/contact/:id', verifyJWT, async (req, res) => {
+            const { id } = req.params;
+            const updateUser = req.body;
+            const query = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: updateUser,
+            };
+            const result = await contactCallection.updateOne(query, updateDoc, options)
+            res.send(result)
+        });
+        app.delete('/contact/:id', verifyJWT, async (req, res) => {
+            const { id } = req.params;
+            const query = { _id: ObjectId(id) }
+            const result = await contactCallection.deleteOne(query)
+            res.send(result)
+        });
+        
     }
-    finally {
-
+    catch {
+        
     }
 }
 run().catch(console.dir);
